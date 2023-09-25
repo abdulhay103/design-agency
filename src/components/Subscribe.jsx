da7cfdde863d04e91327fe82d2718c377cc8bbe2
abdulhay103 <abdulhay.me@gmail.com>
2023-09-25 23:51:20 +0600

    footer & suscribe done

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,29 +1,30 @@
 export default function Subscribe() {
     return (
-        <section>
+        <section className=" py-20">
             <div className=" container mx-auto">
                 <div className=" w-1/2 mx-auto">
-                    <div className="px-10">
-                        <p className="text-green-500 uppercase font-semibold text-center">
+                    <div className="px-5 pb-10">
+                        <p className="text-green-500 uppercase text-xl font-medium text-center pb-4">
                             Subscribe
                         </p>
-                        <h2 className="text-xl font-bold">
+                        <h2 className="text-3xl font-bold capitalize text-center">
                             Subscribe to get the latest news about us
                         </h2>
-                        <p>
+                        <p className=" text-zinc-500 text-sm text-center">
                             Please drop your email below to get daily update
                             about what we do
                         </p>
                     </div>
-                    <div>
+                    <div className=" flex border border-black rounded-md">
                         <input
+                            className=" w-4/5 px-6 py-4 focus:outline-none rounded-md placeholder:text-zinc-500"
                             type="email"
                             name="email"
                             id="email"
                             placeholder="Enter Your Email Address"
                         />
                         <button
-                            className=" bg-orange-600 text-black"
+                            className=" bg-orange-600 w-1/5 text-white rounded-md m-1"
                             type="submit"
                         >
                             Subscribe
